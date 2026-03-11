f8fd95a68186e11deff3ffa6462fa73e1e1626d0
Tuan Anh <tuananh15213@gmail.com>
2026-03-11 09:08:09 +0700

    Reconstruct to Node.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set EJS as the template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log('Press Ctrl+C to stop.');
+});
